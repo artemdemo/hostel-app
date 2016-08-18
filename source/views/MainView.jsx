66c3a89359772fd5ef562782f92c57e80882839a
Artem Demo <artemdemo@gmail.com>
2016-08-18 22:23:03 +0300

    main page

--- source/views/MainView.jsx
+++ source/views/MainView.jsx
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { Page } from 'react-onsenui';
 import { Link } from 'react-router';
 
+import './main-view.less';
+
 export class MainView extends Component {
     render() {
         return (
             <Page>
-                <Link to='/coming' className='button'>Book room</Link>
+                <div className='page-inner-content
+                                page-inner-content_main-view'>
+                    <div className='main-view__title'>
+                        Hostel App
+                    </div>
+                    <div className='main-view__book-room'>
+                        <Link to='/coming'
+                              className='button
+                                         button--large
+                                         button_theme'>
+                            Book room
+                        </Link>
+                    </div>
+                </div>
             </Page>
         );
     }
